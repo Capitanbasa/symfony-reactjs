c3c9c68f6f020733611d466a73d91dda879001df
Val <val@admins-MacBook-Pro.local>
2020-02-28 16:33:33 +0800

    create a DataFixtures class that create BlogPost Dummy Data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first Fixture Post');
+        $blogPost->setPublished("2020-02-28 10:10:10");
+        $blogPost->setContent("You've already seen how the repository object allows you to run basic queries without any work");
+        $blogPost->setAuthor('Hercival Aragon');
+        $blogPost->setSlug('a-first-fixture-post');
 
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second Fixture Post');
+        $blogPost->setPublished("2020-03-01 10:11:59");
+        $blogPost->setContent("Already allows you to run basic queries without any work");
+        $blogPost->setAuthor('Hercival Aragon');
+        $blogPost->setSlug('a-second-fixture-post');
+        $manager->persist($blogPost);
         $manager->flush();
     }
 }
